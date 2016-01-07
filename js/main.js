e8cc5cce4feb4bd8957215e39b93e5797baf592e
mitstark13 <starkeymitchell3@gmail.com>
2016-01-07 17:02:18 -0600

    next commit

--- js/main.js
+++ js/main.js
@@ -15,9 +15,26 @@ var weatherURL = "http://api.openweathermap.org/data/2.5/weather?"
 var weatherApiKey = "appid=316a2b2cad6f5950838210609c099692";
 
 var latLong = {};
-if(navigator.geolocation) {
-  	navigator.geolocation.getCurrentPosition(function(position) {
-  		latLong.lat = position.coords.latitude;
-  		latLong.long = position.coords.longitude;
-  	});
-};
+var city;
+
+$('.currentLocation').click(function() {
+  if(navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(function(position) {
+    		latLong.lat = position;
+    		latLong.long = position.coords.longitude;
+    	});
+  };
+});
+
+$('.newCity').click(function() {
+  if ($('.options input').val().indexOf(',') != -1) {
+    var city = $('.options input').val();
+    $('.splash i').css('opacity', '0');
+    alert(city);
+  } else {
+    $('.splash i').css('opacity', '1');
+  };
+})
+
+
+
