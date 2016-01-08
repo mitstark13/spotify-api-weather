44b6d56f57eeaf076eed9676548fe7f70d2afad1
mitstark13 <starkeymitchell3@gmail.com>
2016-01-08 14:34:15 -0600

    all info in the js

--- js/main.js
+++ js/main.js
@@ -18,11 +18,6 @@ var latLong = [];
 var currentCity;
 var newCity;
 
-var temp;
-var weather;
-var weatherDescription;
-var hour;
-
 function getLocation() {
   if(navigator.geolocation) {
     	navigator.geolocation.getCurrentPosition(function(position) {
@@ -55,45 +50,90 @@ function getWeatherInfo() {
 function showWeatherInfo(data) {
   console.log(data);
   $('.currentCity').html(data.name);
-  temp = data.main.temp;
+  temp = ((data.main.temp * (9 / 5)) - 459.67);
   weather = data.weather[0].main;
   weatherDescription = data.weather[0].description;
   hour = new Date($.now()).getHours();
+  weatherToMusic();
 }
+
+var temp;
+var weather;
+var weatherDescription;
+var hour;
 
 getLocation();
 
-var dance = '.6';
-var energy = '.5';
-var familiar = '.65';
-var duration;
-var tempo;
+var dance = 0.5;
+var energy = 0.5;
+var familiar = 0.64;
+var genre = 'rap';
+var genre2 = 'pop';
+var variety = ((Math.random() * 0.7) + 0.3).toFixed(2);
 
+function weatherToMusic() {
+  if ((weather == 'Rain') || (weather == 'Snow')) {
+    dance -= 0.1;
+    energy -= 0.1;
+  } else {
+    dance += 0.1;
+    energy += 0.1;
+  }
+  if ((temp < 40) || (temp > 75)) {
+    dance -= 0.05;
+    energy -= 0.05;
+  } 
+  if (hour < 5) {
+    dance -= 0.1;
+    energy -= 0.1;
+  } else if ((5 <= hour) && (hour <= 10) || (15<= hour) && (hour <= 18)) {
+    dance += 0.1;
+    energy += 0.1;
+  } else if (hour > 18){
+    dance += 0.2;
+    energy += 0.2;
+  }
+
+  console.log('energy: ' + energy);
+  console.log('dance: ' + dance);
+
+  if (energy < 0.1) {
+    energy = 0.1;
+  }
+  if (dance < 0.1) {
+    dance = 0.1;
+  }
+  if (energy > 0.9) {
+    energy = 0.9;
+  }
+  if (dance > 0.9) {
+    dance = 0.9;
+  }
+
+  getSpotifyPlaylist();
+}
 
 function getSpotifyPlaylist() {
   $.ajax({
-      url: echoURL + echoApiKey,
+      url: echoURL + echoApiKey + "&genre=" + genre + "&genre=" + genre2,
       data: {
-        genre: 'dance%2Bpop',
         format: 'json',
         results: '20',
-        type: 'genre-radio'
-        // start: '900'
+        type: 'genre-radio',
+        artist_min_familiarity: familiar,
+        min_danceability: (dance - 0.1),
+        max_danceability: (dance + 0.1),
+        min_energy: (energy - 0.1),
+        max_energy: (energy + 0.1),
+        bucket: 'audio_summary',
+        variety: variety
       },
       success: function (response) {
           console.log(response);
           // resultsPlaceholder.innerHTML = template(response);
       }
   });
+  console.log(variety);
 }
-
-getSpotifyPlaylist();
-
-
-
-
-// http://developer.echonest.com/api/v4/playlist/static?api_key=FIBUHDRKELAT3EEOJ&genre=dance%2Bpop&format=json&results=20&type=genre-radio
-// http://developer.echonest.com/api/v4/playlist/static?api_key=FIBUHDRKELAT3EEOJ&genre=dance+pop&format=json&results=20&type=genre-radio
-
 
 
